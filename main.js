9a5d45b5514fd6470ddc0d3f0726f7a53815ca0c
xtian0924 <49124522+xtian0924@users.noreply.github.com>
2019-04-03 11:03:55 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var scroll = new SmoothScroll('a[href*="#"]');
+var prevScrollpos = window.pageYOffset;
+
+// Hide Nav
+
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("header").style.top = "0";
+    } else {
+        document.getElementById("header").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+}
